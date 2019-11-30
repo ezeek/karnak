e3b55e6120cfc19bb5cdbec054b80a6ef86950b4
workit247 <eli.altman9@gmail.com>
2019-11-29 20:14:55 -0800

    refactor and hardcode for dash/sh limitations

--- los16/vendor/bin/alsa_trigger/headphone_unplugged.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-
-alsa_amixer cset name='Ext_Speaker_Amp_Switch' 1;
-alsa_amixer cset name='Ext_Headphone_Amp_Switch' 0;
--- los16/vendor/bin/alsa_trigger/platform_init.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-
-alsa_amixer cset name='Audio_Amp_L_Switch' 1;
-alsa_amixer cset name='Audio_Amp_R_Switch' 1;
-alsa_amixer cset name='Ext_Headphone_Amp_Switch' 1;
-alsa_amixer cset name='Ext_Speaker_Amp_Switch' 0;
